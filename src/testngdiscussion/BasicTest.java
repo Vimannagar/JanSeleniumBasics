1920b005eafb1eeb6103dc35a013a273b044fce0
= <=>
2023-04-16 09:11:45 +0530

    TestNg discussion

---
+++ src/testngdiscussion/BasicTest.java
@@ -0,0 +1,28 @@
+package testngdiscussion;
+
+import org.testng.annotations.Test;
+
+public class BasicTest {
+	
+	@Test
+	public void login()
+	{
+		System.out.println("login test case");
+	}
+	
+	
+	@Test
+	public void home()
+	{
+		System.out.println("home test case");
+	}
+	
+	@Test
+	public void logout()
+	{
+		System.out.println("Logout test case");
+	}
+
+	
+	
+}
