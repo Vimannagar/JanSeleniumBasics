0950a569dc637934ddd9dde344cd961c4005c128
= <=>
2023-05-04 09:06:43 +0530

    pom

--- src/webtable/WebTableHandling.java
+++ src/webtable/WebTableHandling.java
@@ -16,14 +16,24 @@ public class WebTableHandling {
 	public void getAllValues()
 	{
 	WebDriver	driver = BaseTest.launchBrowser("https://demo.guru99.com/test/web-table-element.php");
-		
+		int i = 1;
 		List<WebElement> elements = driver.findElements(By.xpath("//*[@class='dataTable']//tbody//tr//td"));
 	
 		for(WebElement element :elements )
 		{
 		String	text = element.getText();
 		
-		System.out.println(text);
+		System.out.print(text+" ");
+		
+		if(i == 5)
+		{
+			System.out.println();
+			i= 1;
+		}
+		else
+		{
+			i++;
+		}
 		}
 	
 	
